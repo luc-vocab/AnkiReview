7bee6a95bb91bdf23363d56a57fb9af7aab32742
luc wastiaux <luc.wastiaux@airpost.net>
2018-07-14 18:00:25 +0800

    query list of decks

--- app/src/main/java/com/luc/ankireview/AnkiReviewActivity.java
+++ app/src/main/java/com/luc/ankireview/AnkiReviewActivity.java
@@ -2,12 +2,42 @@ package com.luc.ankireview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.database.Cursor;
+import java.util.HashMap;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.ichi2.anki.FlashCardsContract;
+
+
 
 public class AnkiReviewActivity extends AppCompatActivity {
+
+    private static final String TAG = "AnkiReviewActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anki_review);
+
+        Log.d(TAG, "hello world");
+
+        Cursor decksCursor = getContentResolver().query(FlashCardsContract.Deck.CONTENT_ALL_URI, null, null, null, null);
+        if (decksCursor.moveToFirst()) {
+            HashMap decks = new HashMap();
+            do {
+                long deckID = decksCursor.getLong(decksCursor.getColumnIndex(FlashCardsContract.Deck.DECK_ID));
+                String deckName = decksCursor.getString(decksCursor.getColumnIndex(FlashCardsContract.Deck.DECK_NAME));
+                try {
+                    JSONObject deckOptions = new JSONObject(decksCursor.getString(decksCursor.getColumnIndex(FlashCardsContract.Deck.OPTIONS)));
+                    JSONArray deckCounts = new JSONArray(decksCursor.getString(decksCursor.getColumnIndex(FlashCardsContract.Deck.DECK_COUNTS)));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                decks.put(deckID, deckName);
+            } while (decksCursor.moveToNext());
+        }
     }
+
 }
