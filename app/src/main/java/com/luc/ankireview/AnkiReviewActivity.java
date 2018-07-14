76a04359056aaad7bdcefa0493e15c7915a460dd
luc wastiaux <luc.wastiaux@airpost.net>
2018-07-14 17:14:36 +0800

    initial import

---
+++ app/src/main/java/com/luc/ankireview/AnkiReviewActivity.java
@@ -0,0 +1,13 @@
+package com.luc.ankireview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnkiReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anki_review);
+    }
+}
