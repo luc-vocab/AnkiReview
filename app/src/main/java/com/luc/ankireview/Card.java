fc88de648a9eef316f85570827134f3cbe82f073
Luc Wastiaux <luc.wastiaux@xsmail.com>
2018-09-14 23:05:10 +0800

    extract sound and replace answer content

--- app/src/main/java/com/luc/ankireview/Card.java
+++ app/src/main/java/com/luc/ankireview/Card.java
@@ -1,19 +1,40 @@
 package com.luc.ankireview;
 
+import android.util.Log;
+
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Card {
+
+    public static Pattern s_soundPattern = Pattern.compile("\\[sound\\:([^\\[\\]]*)\\]");
+    private static final String TAG = "Card";
+
+
     public Card(long noteId, int cardOrd, String question, String answer) {
         m_noteId = noteId;
         m_cardOrd = cardOrd;
         m_question = question;
-        m_answer = answer;
+
+        // does the answer content have a sound ?
+        m_answer = filterSound(answer);
     }
 
-    private long m_noteId;
-    private int m_cardOrd;
-    private String m_question;
-    private String m_answer;
+    private String filterSound( String content ) {
+        Matcher matcher = s_soundPattern.matcher(content);
+        // While there is matches of the pattern for sound markers
+        while (matcher.find()) {
+            String sound = matcher.group(1);
+            Log.v(TAG, "sound: " + sound);
+            m_answerSound = sound;
+        }
+
+        content = matcher.replaceAll("");
+
+        return content;
+    }
 
     public String getQuestion() {
         return m_question;
@@ -37,4 +58,10 @@ public class Card {
 
         return Objects.hash(m_noteId, m_cardOrd);
     }
+
+    private long m_noteId;
+    private int m_cardOrd;
+    private String m_question;
+    private String m_answer;
+    private String m_answerSound;
 }
