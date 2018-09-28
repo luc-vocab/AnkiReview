9ca9ad3e0830379485259d309944ade0403791ef
Luc Wastiaux <luc.wastiaux@xsmail.com>
2018-09-28 21:56:09 +0800

    prototype v2

---
+++ app/src/main/java/com/luc/ankireview/CardPrototypeActivity.java
@@ -0,0 +1,12 @@
+package com.luc.ankireview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CardPrototypeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.review_card_prototype2);
+    }
+}
