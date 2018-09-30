c82c2a929cf0a98b1be5a4d21dbf2f69c05ec38e
Luc Wastiaux <luc.wastiaux@xsmail.com>
2018-09-30 12:14:28 +0800

    subclass CardView to create custom views

---
+++ app/src/main/java/com/luc/ankireview/AnswerCard.java
@@ -0,0 +1,15 @@
+package com.luc.ankireview;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class AnswerCard extends CardView {
+    public AnswerCard(Context context){
+        super(context);
+    }
+
+    public AnswerCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/luc/ankireview/QuestionCard.java
@@ -0,0 +1,15 @@
+package com.luc.ankireview;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class QuestionCard extends CardView {
+    public QuestionCard(Context context){
+        super(context);
+    }
+
+    public QuestionCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
