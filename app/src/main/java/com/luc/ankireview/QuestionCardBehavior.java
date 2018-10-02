fbc478c085a74d6c7581da840693bb3452f74794
Luc Wastiaux <luc.wastiaux@xsmail.com>
2018-10-02 11:43:28 +0800

    properly compute spacing

--- app/src/main/java/com/luc/ankireview/QuestionCardBehavior.java
+++ app/src/main/java/com/luc/ankireview/QuestionCardBehavior.java
@@ -8,6 +8,8 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class QuestionCardBehavior extends CoordinatorLayout.Behavior<QuestionCard> {
     private static final String TAG = "QuestionCardBehavior";
@@ -18,30 +20,60 @@ public class QuestionCardBehavior extends CoordinatorLayout.Behavior<QuestionCar
 
     }
 
-    /*
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, NestedScrollView child, View dependency) {
+    public boolean layoutDependsOn(CoordinatorLayout parent, QuestionCard child, View dependency) {
         Log.v(TAG, "layoutDependsOn " + dependency.getClass().toString());
         if( dependency.getId() == R.id.inner_scrollview) {
             return true;
         }
         return false;
     }
-    */
 
     @Override
     public boolean onLayoutChild (CoordinatorLayout parent,
                                   QuestionCard child,
                                   int layoutDirection) {
-        Log.v(TAG, "onLayoutChild");
+        parent.onLayoutChild(child, layoutDirection);
 
-        int totalHeight = parent.getHeight();
-        int questionHeight = child.getHeight();
+        if( ! m_initialLayoutDone) {
+            // center the question vertically
 
-        int yPosition = totalHeight/2 - questionHeight/2;
+            int totalWindowHeight = parent.getHeight();
+            int questionHeight = child.getHeight();
 
-        Log.v(TAG,"onLayoutChild, setting Y position to " + yPosition + " totalHeight: " + totalHeight);
-        child.setY(yPosition);
+            int yPosition = totalWindowHeight/2 - questionHeight/2;
+
+            Log.v(TAG,"onLayoutChild, setting Y position to " + yPosition + " totalHeight: " + totalWindowHeight);
+            child.setY(yPosition);
+
+            // place the answer just below the screen threshold
+            FrameLayout topSpacer = (FrameLayout) parent.findViewById(R.id.answer_top_spacer);
+
+            //LinearLayout.LayoutParams
+            LinearLayout.LayoutParams topSpacerLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, totalWindowHeight);
+            //topSpacer.setMinimumHeight(totalWindowHeight);
+            topSpacer.setLayoutParams(topSpacerLayoutParams);
+
+            // calculate how much the answer can scroll up
+            AnswerCard answer = parent.findViewById(R.id.answer_card);
+            int answerHeight = answer.getHeight();
+
+            // get margins
+            ViewGroup.MarginLayoutParams questionMarginParams = (ViewGroup.MarginLayoutParams) child.getLayoutParams();
+            ViewGroup.MarginLayoutParams answerMarginParams = (ViewGroup.MarginLayoutParams) answer.getLayoutParams();
+
+            int combinedHeight = questionHeight + questionMarginParams.bottomMargin + answerMarginParams.topMargin + answerHeight;
+            int bottomSpacerHeight = totalWindowHeight / 2 - combinedHeight / 2;
+
+
+            FrameLayout bottomSpacer = (FrameLayout) parent.findViewById(R.id.answer_bottom_spacer);
+            LinearLayout.LayoutParams bottomSpacerLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, bottomSpacerHeight);
+            bottomSpacer.setLayoutParams(bottomSpacerLayoutParams);
+
+            m_initialLayoutDone = true;
+
+            return true;
+        }
 
         return false;
     }
@@ -125,6 +157,9 @@ public class QuestionCardBehavior extends CoordinatorLayout.Behavior<QuestionCar
         }
 
     }
+
+
+    private boolean m_initialLayoutDone = false;
 
 
 }
