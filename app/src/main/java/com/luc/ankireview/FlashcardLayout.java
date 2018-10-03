6ea0a3066a1f3960deb2e6f9a08e827a94c0a9b8
Luc Wastiaux <luc.wastiaux@xsmail.com>
2018-10-03 19:47:00 +0800

    add touch layer and gesture detection for a flashcard layout

--- app/src/main/java/com/luc/ankireview/FlashcardLayout.java
+++ app/src/main/java/com/luc/ankireview/FlashcardLayout.java
@@ -2,10 +2,17 @@ package com.luc.ankireview;
 
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.GestureDetectorCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class FlashcardLayout extends CoordinatorLayout {
+    private static final String TAG = "FlashcardLayout";
 
     public FlashcardLayout(Context context) {
         super(context);
@@ -31,9 +38,41 @@ public class FlashcardLayout extends CoordinatorLayout {
         init(context);
     }
 
+    class FlingGestureDetector extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            // need to return true to indicate interest in the event, otherwise the
+            // remaining events won't be sent to us
+
+            Log.v(TAG, "FlingGestureDetector.onDown");
+
+            return true;
+        }
+
+
+    }
+
+    private View.OnTouchListener m_gestureListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            // run through gesture detector
+            if (m_detector.onTouchEvent(event)) {
+                return true;
+            }
+            return false;
+        }
+    };
 
     private void init(Context context) {
         inflate(context, R.layout.flashcard, this);
+
+        FrameLayout touchLayer = findViewById(R.id.flashcard_touch_layer);
+
+        // set touch listener
+        m_detector = new GestureDetectorCompat(context, new FlashcardLayout.FlingGestureDetector());
+        touchLayer.setOnTouchListener(m_gestureListener);
+
     }
 
     public void setCard(Card card) {
@@ -45,5 +84,8 @@ public class FlashcardLayout extends CoordinatorLayout {
         answerText.setText(card.getAnswerSimple());
 
     }
+
+    // gesture detection
+    private GestureDetectorCompat m_detector;
 
 }
