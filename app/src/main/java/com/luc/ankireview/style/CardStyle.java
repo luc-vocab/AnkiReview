e8864e1dff5e9ff3e79cfa626012fa4004c63b73
Luc Wastiaux <luc.wastiaux@xsmail.com>
2019-02-02 22:20:14 +0800

    don't do anything for empty strings. fixes #7

--- app/src/main/java/com/luc/ankireview/style/CardStyle.java
+++ app/src/main/java/com/luc/ankireview/style/CardStyle.java
@@ -229,9 +229,10 @@ public class CardStyle implements Serializable {
 
 
     private CharSequence removeTrailingLineReturns(CharSequence text) {
-
-        while (text.charAt(text.length() - 1) == '\n') {
-            text = text.subSequence(0, text.length() - 1);
+        if( text.length() > 0 ) {
+            while (text.charAt(text.length() - 1) == '\n') {
+                text = text.subSequence(0, text.length() - 1);
+            }
         }
         return text;
     }
