94907e224104d4379ffc4d3358ffa1ae2b1877ac
Luc Wastiaux <luc.wastiaux@xsmail.com>
2019-10-27 13:24:54 +0800

    width is now defined through code

--- app/src/main/java/com/luc/ankireview/display/ReviewConstraintSetManager.java
+++ app/src/main/java/com/luc/ankireview/display/ReviewConstraintSetManager.java
@@ -9,10 +9,26 @@ import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 public class ReviewConstraintSetManager {
+    public static float WIDTH = 0.9f;
 
     public static void applyConstraintSetConstants(MotionLayout reviewMotionLayout) {
+        Resources res = reviewMotionLayout.getResources();
+
         ConstraintSet questionShownConstraintSet = reviewMotionLayout.getConstraintSet(R.id.question_shown);
-        questionShownConstraintSet.setMargin(R.id.question_card, ConstraintSet.TOP, dpToPx(40, reviewMotionLayout.getResources()));
+        // questionShownConstraintSet.setMargin(R.id.question_card, ConstraintSet.TOP, dpToPx(40, res));
+
+        setWidth(reviewMotionLayout.getConstraintSet(R.id.question_shown));
+        setWidth(reviewMotionLayout.getConstraintSet(R.id.answer_shown));
+        setWidth(reviewMotionLayout.getConstraintSet(R.id.answer_bad));
+        setWidth(reviewMotionLayout.getConstraintSet(R.id.answer_good));
+        setWidth(reviewMotionLayout.getConstraintSet(R.id.answer_good_offscreen));
+        setWidth(reviewMotionLayout.getConstraintSet(R.id.answer_bad_offscreen));
+    }
+
+    public static void setWidth(ConstraintSet constraintSet) {
+        constraintSet.constrainPercentWidth(R.id.question_card, WIDTH);
+        constraintSet.constrainPercentWidth(R.id.answer_card, WIDTH);
+        constraintSet.constrainPercentWidth(R.id.next_question_card, WIDTH);
     }
 
     public static int dpToPx(int dpDimension, Resources resources) {
