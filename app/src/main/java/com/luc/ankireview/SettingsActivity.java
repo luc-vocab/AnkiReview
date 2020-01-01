9aff1b564a8c2ca16319fcf65ae2b84d6877d22c
Luc Wastiaux <luc.wastiaux@xsmail.com>
2020-01-01 08:44:49 +0800

    use coordinatorlayout, set toolbar title properly

--- app/src/main/java/com/luc/ankireview/SettingsActivity.java
+++ app/src/main/java/com/luc/ankireview/SettingsActivity.java
@@ -3,16 +3,21 @@ package com.luc.ankireview;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_layout);
+        setContentView(R.layout.activity_settings);
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.settings_layout, new SettingsFragment())
+                .replace(R.id.preferences_view, new SettingsFragment())
                 .commit();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        toolbar.setTitle(R.string.settings_toolbar_title);
+        setSupportActionBar(toolbar);
     }
 
 }
